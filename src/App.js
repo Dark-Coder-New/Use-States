3895ae0b199fbc0b3629afe6e38a5627ba6e6d09
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-12 21:07:51 +0530

    form2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import States1 from "./Components/States1";
 import Counter from "./Components/Counter";
 import States2 from "./Components/States2";
 import Form1 from "./Components/Form1";
+import Forms2 from "./Components/Forms2";
 
 const App = () => {
 
@@ -13,7 +14,8 @@ const App = () => {
             {/* <States1 /> */}
             {/* <Counter /> */}
             {/* <States2 /> */} 
-            <Form1 />
+            {/* <Form1 /> */}
+            <Forms2 />
         </div>
     )
 }
